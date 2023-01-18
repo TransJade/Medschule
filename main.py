818d7cf5a3f23d5dd457af6bbda89f9e2b2cea74
Jade <122915473+TransJade@users.noreply.github.com>
2023-01-18 11:34:39 +0100

    Aktualisiere main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,3 +1,32 @@
+manuel = 0
+Licht = 0
+
+def on_button_a():
+    if manuel == 1:
+        if Licht == 1:
+            basic.set_led_color(basic.rgb(0, 0, 0))
+        if Licht == 0:
+            basic.set_led_color(Colors.ORANGE)
+input.on_button_event(Button.A, input.button_event_click(), on_button_a)
+
+def on_button_b():
+    if manuel == 0:
+        manuel = 1
+    else:
+        pass
+input.on_button_event(Button.B, input.button_event_click(), on_button_b)
+
 def on_forever():
-    pass
+    global Licht
+    if manuel == 0:
+        if input.light_level() < 50:
+            basic.set_led_color(Colors.ORANGE)
+            Licht = 1
+        else:
+            basic.set_led_color(basic.rgb(0, 0, 0))
+            Licht = 0
 basic.forever(on_forever)
+
+def on_forever2():
+    print(manuel)
+forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,3 +1,41 @@
+let manuel = 0
+let Licht = 0
+input.onButtonEvent(Button.A, input.buttonEventClick(), function on_button_a() {
+    if (manuel == 1) {
+        if (Licht == 1) {
+            basic.setLedColor(basic.rgb(0, 0, 0))
+        }
+        
+        if (Licht == 0) {
+            basic.setLedColor(Colors.Orange)
+        }
+        
+    }
+    
+})
+input.onButtonEvent(Button.B, input.buttonEventClick(), function on_button_b() {
+    let manuel: number;
+    if (manuel == 0) {
+        manuel = 1
+    } else {
+        
+    }
+    
+})
 basic.forever(function on_forever() {
     
+    if (manuel == 0) {
+        if (input.lightLevel() < 50) {
+            basic.setLedColor(Colors.Orange)
+            Licht = 1
+        } else {
+            basic.setLedColor(basic.rgb(0, 0, 0))
+            Licht = 0
+        }
+        
+    }
+    
+})
+forever(function on_forever2() {
+    console.log(manuel)
 })
